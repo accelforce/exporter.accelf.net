db69de81391c02a0afc871f9a4f8b5a5c3570b1b
kyori19 <kyori.dis@gmail.com>
2019-11-22 22:06:10 +0900

    Initial commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# node_exporter install script
+# curl -fsSL https://exporter.accelf.net/ | sh
+
+VERSION="0.18.1"
+
+do_install() {
+  echo "node_exporter binary install script"
+  echo "Create user prometheus"
+  sudo useradd -ms /bin/bash prometheus
+  echo "Download binary file"
+  sudo curl -fsSL "https://github.com/prometheus/node_exporter/releases/download/v$VERSION/node_exporter-$VERSION.linux-amd64.tar.gz" | tar -xzO "node_exporter-$VERSION.linux-amd64/node_exporter" > /home/prometheus/node_exporter
+  sudo chmod +x /home/prometheus/node_exporter
+  sudo chown prometheus /home/prometheus/node_exporter
+  echo "Download systemd config file"
+  sudo curl -fsSL https://exporter.accelf.net/node_exporter.service -o /etc/systemd/system/node_exporter.service
+  sudo systemctl daemon-reload
+  sudo systemctl enable node_exporter
+  sudo systemctl start node_exporter
+  echo "Install completed"
+  curl -fsSL localhost:9100/metrics | grep -e '^node_exporter_build_info'
+}
+
+do_install
